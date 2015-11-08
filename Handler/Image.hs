48876fdcb511d754b0f6e43980556e5e6acaf04e
Frederik Galle <frederikgalle@fastmail.fm>
2015-11-08 12:27:09 +0100

    Add images page

--- Handler/Image.hs
+++ Handler/Image.hs
@@ -3,4 +3,7 @@ module Handler.Image where
 import Import
 
 getBlogImagesR :: Handler Html
-getBlogImagesR = undefined
+getBlogImagesR = do
+    images <- runDB $ selectList [] [Desc ImageUploaded]
+    thumbsDir <- appThumbsDir <$> getsYesod appSettings
+    defaultLayout $(widgetFile "images")
