73198b1cd2c2e4880452600d7f914d18c4cf11c3
Frederik Galle <frederikgalle@fastmail.fm>
2015-11-13 20:03:00 +0100

    Add authentication and authorization

--- Foundation.hs
+++ Foundation.hs
@@ -4,7 +4,7 @@ import Import.NoFoundation
 import Database.Persist.Sql (ConnectionPool, runSqlPool)
 import Text.Hamlet          (hamletFile)
 import Text.Jasmine         (minifym)
-import Yesod.Auth.BrowserId (authBrowserId)
+import Yesod.Auth.HashDB    (authHashDB)
 import Yesod.Auth.Message   (AuthMessage (InvalidLogin))
 import Yesod.Default.Util   (addStaticContentExternal)
 import Yesod.Core.Types     (Logger)
@@ -77,8 +77,13 @@ instance Yesod App where
     isAuthorized (AuthR _) _ = return Authorized
     isAuthorized FaviconR _ = return Authorized
     isAuthorized RobotsR _ = return Authorized
-    -- Default to Authorized for now.
-    isAuthorized _ _ = return Authorized
+    isAuthorized route _writable
+        | "admin" `member` routeAttrs route = do
+            muser <- maybeAuthId
+            case muser of
+                Nothing -> return AuthenticationRequired
+                Just _ -> return Authorized
+        | otherwise = return Authorized
 
     -- This function creates static content files in the static folder
     -- and names them based on a hash of their content. This allows
@@ -136,11 +141,11 @@ instance YesodAuth App where
     authenticate creds = runDB $ do
         x <- getBy $ UniqueUser $ credsIdent creds
         return $ case x of
-            Just (Entity uid _) -> Authenticated uid
+            Just (Entity userId _) -> Authenticated userId
             Nothing -> UserError InvalidLogin
 
     -- You can add other plugins like BrowserID, email or OAuth here
-    authPlugins _ = [authBrowserId def]
+    authPlugins _ = [authHashDB (Just . UniqueUser)]
 
     authHttpManager = getHttpManager
 
--- Model.hs
+++ Model.hs
@@ -6,6 +6,7 @@ import Text.Markdown (Markdown)
 import Yesod.Text.Markdown ()
 import Data.Aeson.TH (deriveJSON, defaultOptions)
 import Data.Time.Clock (diffUTCTime)
+import Yesod.Auth.HashDB (HashDBUser(..))
 
 -- You can define all of your database entities in the entities file.
 -- You can find more information on persistent and how to declare entities
@@ -13,6 +14,10 @@ import Data.Time.Clock (diffUTCTime)
 -- http://www.yesodweb.com/book/persistent/
 share [mkPersist sqlSettings, mkMigrate "migrateAll"]
     $(persistFileWith lowerCaseSettings "config/models")
+
+instance HashDBUser User where
+    userPasswordHash = userPassword
+    setPasswordHash h u = u { userPassword = Just h }
 
 data ImageUpdate = ImageUpdate
     { visible :: Bool
