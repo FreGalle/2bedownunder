87658dfd9a8b9e4259fb75dc2d1a579f0aceffea
Frederik Galle <frederikgalle@fastmail.fm>
2015-11-21 23:02:37 +0100

    Root now shows last post

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -4,4 +4,9 @@ import Import
 import Handler.Entry
 
 getHomeR :: Handler Html
-getHomeR = getBlogEntriesR
+getHomeR = do
+    now <- liftIO getCurrentTime
+    mEntryId <- runDB $ selectFirst [EntryPosted <. Just now] [Desc EntryPosted]
+    case mEntryId of
+        Just (Entity entryId _) -> getBlogEntryR entryId
+        Nothing -> getBlogEntriesR
