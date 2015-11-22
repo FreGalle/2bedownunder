1c95fa2383a8eba3e7dee0a7d78612ba25f38369
Frederik Galle <frederikgalle@fastmail.fm>
2015-11-22 17:55:46 +0100

    Add sitemap

--- Handler/Common.hs
+++ Handler/Common.hs
@@ -3,6 +3,8 @@ module Handler.Common where
 
 import Data.FileEmbed (embedFile)
 import Import
+import Yesod.Sitemap (sitemap, SitemapUrl (..), SitemapChangeFreq (..))
+import qualified Data.Conduit.List as CL
 
 -- These handlers embed files in the executable at compile time to avoid a
 -- runtime dependency, and for efficiency.
@@ -14,3 +16,15 @@ getFaviconR = return $ TypedContent "image/x-icon"
 getRobotsR :: Handler TypedContent
 getRobotsR = return $ TypedContent typePlain
                     $ toContent $(embedFile "config/robots.txt")
+
+getSitemapR :: Handler TypedContent
+getSitemapR = do
+    now <- liftIO getCurrentTime
+    sitemap $ runDBSource $ do
+        yield $ SitemapUrl HomeR Nothing (Just Daily) (Just 1.0)
+
+        yield $ SitemapUrl BlogEntriesR Nothing (Just Daily) (Just 0.8)
+        yield $ SitemapUrl BlogImagesR Nothing (Just Daily) (Just 0.8)
+
+        selectSource [EntryPosted <. Just now] [Asc EntryPosted] $= CL.mapMaybeM (\(Entity eId _) ->
+            return $ Just $ SitemapUrl (BlogEntryR eId) Nothing (Just Weekly) (Just 0.5))
