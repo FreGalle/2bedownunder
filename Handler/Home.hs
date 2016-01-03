57921012fa667902c620db7fc23ac2d88064e6cf
Frederik Galle <frederikgalle@fastmail.fm>
2016-01-03 15:45:26 +1100

    Make homepage just go to blog posts

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -4,9 +4,4 @@ import Import
 import Handler.Entry
 
 getHomeR :: Handler Html
-getHomeR = do
-    now <- liftIO getCurrentTime
-    mEntryId <- runDB $ selectFirst [EntryPosted <. Just now] [Desc EntryPosted]
-    case mEntryId of
-        Just (Entity entryId _) -> getBlogEntryR entryId
-        Nothing -> getBlogEntriesR
+getHomeR = getBlogEntriesR
